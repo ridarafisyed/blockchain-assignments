e27563678224a0c9e7a8da3f47754c85e05b700e
ridarafisyed <ridarafisyed@yahoo.com>
2021-02-05 10:53:30 +0500

    adding task4

---
+++ assignment4/task4/task4.js
@@ -0,0 +1,12 @@
+//  task4
+
+var text = "example@example.com";
+document.write(text);
+var n = text.indexOf("@");
+var dot = text.indexOf(".");
+
+var len = text.length;
+
+if((n< dot) && (len > dot)){
+    
+}
